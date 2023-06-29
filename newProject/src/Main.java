63d25593edfb86f0e51b663be87b19d17c14a1eb
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-06-29 08:07:24 +0530

    setup git repo

--- newProject/src/Main.java
+++ newProject/src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello World");
 
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+
+        if(s.equals("Caleb")) {
+            System.out.println("Hey Caleb");
+        }
+        else if (s.equals("Sally")){
+            System.out.println("Hey Sally");
+        }
+        else {
+            System.out.println("You're not welcome here");
+        }
+
+        scanner.close();
     }
 }
